e63891e593f67bbf2369797ed17d55370626e1b1
MrGreen <fina202418@st.forum.ac.jp>
2025-01-21 10:49:05 +0900

    commit A

--- mycode.py
+++ mycode.py
@@ -3,5 +3,6 @@ print("my second line")
 print("once again master changed the edit from japan team")
 
 print("change for the second commit");
+print("change for the thrid commit");
 print("change for the fourt commit");
 print("change for the five commit");
